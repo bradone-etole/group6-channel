3c93b427c74b6c0b0ea1a0252b26f71a2240e73c
bradone-etole <omwitsabradone@gmail.com>
2022-04-07 06:41:26 +0300

    Add files and changes requuired for deployement

--- channel/settings.py
+++ channel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +137,12 @@ CACHES = {
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 MEDIA="/media/"
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_URL = '/static/'
